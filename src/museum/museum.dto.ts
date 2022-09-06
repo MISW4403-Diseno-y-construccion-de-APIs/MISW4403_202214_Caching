cf194ee9614dfcb2846e98d104c5bb18917babb6
José Bocanegra <j.bocanegra@uniandes.edu.co>
2022-09-06 11:52:18 -0500

    Update museum.dto.ts

--- src/museum/museum.dto.ts
+++ src/museum/museum.dto.ts
@@ -1,8 +1,6 @@
 /* eslint-disable prettier/prettier */
 import {IsNotEmpty, IsString, IsUrl} from 'class-validator';
 export class MuseumDto {
-  readonly id: string;
-  
   @IsString()
   @IsNotEmpty()
   readonly name: string;
